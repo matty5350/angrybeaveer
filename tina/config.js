c79a3daf696e7af670f3ffd6198afc68b3006070
WARPED IBUN <84038557+matty5350@users.noreply.github.com>
2026-01-29 10:02:00 +0000

    Update config.js

--- tina/config.js
+++ tina/config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "tinacms";
 import page from "./collections/page";
 import post from "./collections/post";
+import store from "./content/page/store.md";
 
 export const config = defineConfig({
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
@@ -26,7 +27,7 @@ export const config = defineConfig({
     outputFolder: "admin", // within the public folder
   },
   schema: {
-    collections: [page, post],
+    collections: [page, post, store],
   },
 });
 
